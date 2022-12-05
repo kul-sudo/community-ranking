8e2664e6d3077f21865f14e7ce97e3b63c0c7638
kul-sudo <ulyanov.kirill@protonmail.com>
2022-12-05 22:38:49 +0300

    first commit

---
+++ lib/theme.ts
@@ -0,0 +1,40 @@
+import { extendTheme } from '@chakra-ui/react'
+import { mode, GlobalStyleProps } from '@chakra-ui/theme-tools'
+import '@fontsource/quicksand/600.css'
+
+type Breakpoints = {
+  [breakpoint: string]: string
+}
+
+type Fonts = {
+  body: string;
+}
+
+type Config = {
+  initialColorMode: string;
+  useSystemColorMode: boolean;
+}
+
+const breakpoints: Breakpoints = {
+  '1100px': '1100px',
+  '741px': '741px'
+}
+
+const styles = {
+  global: (props: GlobalStyleProps) => ({
+    body: {
+      bg: mode('#d1d1d1', '#141822')(props)
+    }
+  })
+}
+
+const fonts: Fonts = {
+  body: `'Quicksand', sans-serif`
+}
+
+const config: Config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
+}
+
+export default extendTheme({ fonts, config, styles, breakpoints })
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../lib/theme'
+import { useEffect, useState } from 'react'
+
+const Hydrated = ({ children }) => {
+  const [hydration, setHydration] = useState(false)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setHydration(true)
+    }
+  }, [])
+
+  return hydration ? children : null
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Hydrated>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Hydrated>
+  )
 }
